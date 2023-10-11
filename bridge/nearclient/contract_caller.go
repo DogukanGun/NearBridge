42fb3752140e1314d57a845be526eaa96029cd92
cemdenizsel <cem.denizsel@gmail.com>
2023-10-11 11:54:23 +0200

    update inside contract_caller.go

--- bridge/nearclient/contract_caller.go
+++ bridge/nearclient/contract_caller.go
@@ -1,71 +1,58 @@
 package main
 
 import (
-	"crypto/ed25519"
-	"encoding/hex"
+	"encoding/json"
+	"errors"
 	"github.com/aurora-is-near/near-api-go"
 	"log"
 	"math/big"
 	"os"
 )
 
-func main() {
-	// Set environment variable for network choice
-	os.Setenv("NEAR_ENV", "testnet")
-
-	config := near.GetConfig()
-
-	accountID := "c_test.testnet"
-	privateKeyData := "yourPrivateKeyInBytesFormat" // Convert your private key to bytes format if it's in string
-	privateKey := ed25519.NewKeyFromSeed(privateKeyData)
-	// Create a new NEAR connection using the configuration
-	connection := near.NewConnection(config.NodeURL) 
-
-	// Set up the contract details
-	contractID := "your.deployed.contract.id"
-	methodName := "send_message"
-	args := map[string]interface{}{
-		"message": "Hello from Go!",
-	}
-
-	encodedArgs, err := near.Serialize(args) // Assuming Serialize exists in the near package to encode the arguments
-	if err != nil {
-		log.Fatal(err)
-	}
-	functionCallAction := near.Action{
-		Enum: 3,  // For FunctionCall
-		FunctionCall: near.FunctionCall{
-			MethodName: methodName,
-			Args:       encodedArgs,
-			Gas:        10000000000000,
-			Deposit:    big.NewInt(0),
-		},
-	}
-
-	// Create and sign the transaction
-	blockHash, _ := hex.DecodeString("YOUR_BLOCK_HASH") // Replace with an appropriate block hash
-	actions := []near.Action{functionCallAction}
-	publicKeyBytes, _ := hex.DecodeString("YOUR_PUBLIC_KEY_HEX") // Replace with your account's public key in hex format
-	txHash, signedTx, err := near.signTransaction(
-		contractID,
-		account.Nonce + 1, // Incrementing the nonce by 1 for this transaction
-		actions,
-		blockHash,
-		ed25519.PublicKey(publicKeyBytes),
-		account.PrivateKey,  // Assuming this is how you retrieve the private key
-		accountID,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Send the transaction
-	result, err := near.SendTransaction(connection, signedTx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Handle the result
-	log.Printf("Result: %v", result)
+type MyFunctionArgs struct {
+	Message string `json:"message"`
 }
 
+func InteractWithContract(methodName string, message string) (interface{}, error) {
+	//TODO: check more usage of putting keypath in config
+	var config = near.GetConfig()
+	var connection = near.NewConnection(config.NodeURL)
+	accountID := os.Getenv("NEAR_ACCOUNT_ID")
+	if accountID == "" {
+		return nil, errors.New("environment variable NEAR_ACCOUNT_ID is not set")
+	}
+
+	// Load the account using the connection and the provided credentials
+	account, err := near.LoadAccount(connection, config, accountID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert your arguments to a serialized format
+	argsStruct := MyFunctionArgs{Message: message}
+	args, err := json.Marshal(argsStruct) //borsh.Serialize(argsStruct)
+	if err != nil {
+		log.Fatalf("Error serializing arguments: %v", err)
+	}
+
+	// Using the provided FunctionCall inside acccount.go
+	amount := *big.NewInt(0)
+	result, err := account.FunctionCall("c_test.testnet", methodName, args, 10000000000000, amount)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func main() {
+	// Setting up the environment variables for testing
+	os.Setenv("NEAR_ENV", "testnet")
+	os.Setenv("NEAR_ACCOUNT_ID", "c_test.testnet")
+
+	result, err := InteractWithContract("send_message", "Hello from Go_2!")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Result: %v", result)
+}
